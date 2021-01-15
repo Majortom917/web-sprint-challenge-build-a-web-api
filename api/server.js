d35a53dd92a21483f11237c13c0984940f089691
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2021-01-15 01:32:04 -0800

    server init

--- api/server.js
+++ api/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const server = express();
+server.use(express.json())
+
+server.get("/", (req, res) =>{
+    res.staatus(200).json({
+        api: "down"
+    })
+})
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
